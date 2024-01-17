9b3fa9f51c86841dee8a8de78b218da0b4ea07fb
happyhippy78 <happyhippy78@mail.ru>
2024-01-17 20:30:51 +0300

    reload project from site

--- PostUrl/appSite/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import FormUser
-
-@admin.register(FormUser)
-class FormUserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'email', 'age']
-    list_display_links = ['name', 'email']
-    list_filter = ['age']
--- PostUrl/appSite/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.PageHome.as_view(), name = "urlPageHome")
-]
