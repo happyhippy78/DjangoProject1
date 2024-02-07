b25d5c8055063b728505e3988a86692f7ac835df
happyhippy78 <happyhippy78@mail.ru>
2024-02-07 20:55:02 +0300

    add new pages to our project

--- TestProject/appKey/urls.py
+++ TestProject/appKey/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeKey.as_view(), name="urlHomeKey")
+    # path('', views.HomeKey.as_view(), name="urlHomeKey"),
+    path('', views.CategorysPage.as_view(), name="urlCategorys"),
+    path('category/<int:id>', views.ProductPage.as_view(), name="urlProduct"),
+    
 ]
--- TestProject/appKey/views.py
+++ TestProject/appKey/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse # отображение словаря для API
 from django.views import View
-from .models import Product, Category, ProductPhotos
+from .models import Product, Category, ProductPhotos, Subcategory
 
 
 class HomeKey(View):
@@ -24,3 +24,34 @@ class HomeKey(View):
                 title = request.POST['text']).delete(
             )
         return redirect('urlHomeKey')
+    
+class CategorysPage(View):
+    def get(self, request):
+        data_sub = Subcategory.objects.all()
+        data = {}
+        for sub in data_sub:
+            if sub.category.title not in data:
+                data.update({
+                    sub.category.title: {
+                        sub.id : sub.title
+                    }
+                })
+            else:
+                data[sub.category.title].update({
+                    sub.id : sub.title
+                })
+        
+        return render(request, 'appKey/home/index.html', {'object': data})
+    
+class ProductPage(View):
+    def get(self, request, id):
+        sub_category = Subcategory.objects.filter(id=id)
+        if sub_category:
+            data = {
+                'title': sub_category.first().title,
+                'cards': Product.objects.filter(subcategory=id).values('title', 'price', 'id', 'currency')
+                }
+            return render(request, 'appKey/home/product.html', data)
+        else:
+            return redirect('urlCategorys')
+        
